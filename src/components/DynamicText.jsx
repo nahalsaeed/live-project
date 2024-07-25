28febdcc7917913ca15cc84aa61dc40d2739df56
Nahal Saeed <nahalcortechsols@gmail.com>
2024-07-25 10:54:29 +0500

    txt changes

--- src/components/DynamicText.jsx
+++ src/components/DynamicText.jsx
@@ -4,11 +4,14 @@ export default function DynamicText({ heading, text }) {
     return (
         <div className="setCenter">
             <div className="w-[70%] md:w-[45%] lg:w-[40%] xl:w-[37%] 2xl:w-[27%]">
+                <div className="ml-0 md:-ml-[30%]">
+
+              
                 <h1 className="boldHeadings ">
                     {heading}
                 </h1>
                 <p className="text-xs lg:text-sm 2xl:text-lg">{text}</p>
-
+                </div>
             </div>
         </div>
     )
