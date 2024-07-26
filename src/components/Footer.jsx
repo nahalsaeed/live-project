70567e4644b491b21fa400cb388d5fb5d97a0e91
Nahal Saeed <nahalcortechsols@gmail.com>
2024-07-26 11:01:17 +0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,24 @@
+'use client'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "@/app/globals.css";
-import footerlogo from "@/assets/footer-logo.webp"
-import logo from "@/assets/logo.svg"
+import footerlogo from "@/assets/footer-logo.webp";
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Footer() {
+    const [open, setOpen] = useState(null);
+
+    const toggle = (index) => {
+        if (open === index) {
+            setOpen(null);
+        } else {
+            setOpen(index);
+        }
+    };
+
     const footerData = [
         {
+            id: 1,
             title: 'Find us on',
             items: [
                 { text: 'X', link: '#', icon: 'fa-x-twitter' },
@@ -16,6 +28,7 @@ export default function Footer() {
             ],
         },
         {
+            id: 2,
             title: 'Contact us',
             items: [
                 { text: 'Site feedback', link: '#' },
@@ -23,6 +36,7 @@ export default function Footer() {
             ],
         },
         {
+            id: 3,
             title: 'Help',
             items: [
                 { text: 'FAQs', link: '#' },
@@ -33,16 +47,14 @@ export default function Footer() {
                 { text: 'Privacy Policy', link: '#' },
             ],
         },
-
     ];
+
     return (
         <div>
-
-            <div className='bg-[#2D2D2D] text-white h-10 text-sm mt-10 '>
+            <div className='bg-[#2D2D2D] text-white h-10 text-sm mt-10'>
                 <div className='setCenter'>
-                    <div className='w-[85%] mt-3 2xl:w-[50%] '>
-                        <span className='p-1 mr-2 rounded-md bg-[#171717] '>You are here</span> Home
-
+                    <div className='w-[85%] mt-3 2xl:w-[50%]'>
+                        <span className='p-1 mr-2 rounded-md bg-[#171717]'>You are here</span> Home
                     </div>
                 </div>
             </div>
@@ -51,14 +63,17 @@ export default function Footer() {
                 <div className='setCenter'>
                     <div className='w-[82%]'>
                         <div className='flex justify-center items-center h-auto'>
-                            <div className=" grid grid-cols-12  gap-3">
-                                {footerData.map((section, index) => (
-                                    <div key={index} className=" ml-[30%] md:ml-[0%] lg:ml-[0%] text-nowrap col-span-7 order-1 md:order-5 lg:order-none  lg:col-span-2">
-                                        <h2 className="font-bold mb-2">{section.title}</h2>
-                                        <ul className='text-sm'>
+                            <div className="grid grid-cols-12 gap-3 ">
+                                {footerData.map((section) => (
+                                    <div key={section.id} className="col-span-12 md:col-span-6 lg:col-span-2 order-1 lg:order-none border-b border-gray-200 py-2 md:border-none">
+                                        <div className="flex justify-between items-center cursor-pointer" onClick={() => toggle(section.id)}>
+                                            <h2 className="font-bold mb-2">{section.title}</h2>
+                                            <span className="ml-2 block md:hidden">{open === section.id ? '-' : '+'}</span>
+                                        </div>
+                                        <ul className={`text-sm mt-2 transition-all duration-300 ease-in-out ${open === section.id ? 'max-h-screen' : 'max-h-0 overflow-hidden'} md:max-h-screen md:block`}>
                                             {section.items.map((item, idx) => (
-                                                <li key={idx} className="mb-1">
-                                                    <a href={item.link} className="hover:underline text-xs" >
+                                                <li key={idx} className="mb-1 text-left">
+                                                    <a href={item.link} className="hover:underline text-xs">
                                                         {item.icon && <i className={`fab ${item.icon} mr-2`}></i>}
                                                         {item.text}
                                                     </a>
@@ -67,11 +82,14 @@ export default function Footer() {
                                         </ul>
                                     </div>
                                 ))}
-                                <div className='flex flex-col md:flex-row lg:flex-col gap-1 md:gap-10  lg:gap-0 xl:gap-2  xl:flex-row  col-span-12 lg:col-span-6 justify-center items-center md:items-start md:-ml-[10%] lg:ml-[15%]  mt-2'>
-                                    <div >
+
+                                <div className='flex flex-col md:flex-row lg:flex-col gap-1 md:gap-10 lg:gap-0 xl:gap-2 xl:flex-row col-span-12 lg:col-span-6 justify-center items-center md:items-start md:-ml-[10%] lg:ml-[15%] mt-2'>
+                                    <div>
                                         <Image src={footerlogo} alt='logo' height={98} width={98} />
                                     </div>
-                                    <p className='items-start mt-7 text-xl lg:text-2xl text-black text-nowrap'> <span className='font-bold'>Splitdyboat</span> 香港夾船平台 </p>
+                                    <p className='items-start mt-7 text-xl lg:text-2xl text-black text-nowrap'>
+                                        <span className='font-bold'>Splitdyboat</span> 香港夾船平台
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -80,4 +98,4 @@ export default function Footer() {
             </footer>
         </div>
     );
-};
+}
